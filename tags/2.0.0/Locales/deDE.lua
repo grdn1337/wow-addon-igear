8a6110e2f1feadc67189cd4c9f8f8b203f632bf2
grdn <grdn@8ac71d79-9e84-4f73-86c8-66763a5b8044>
2012-07-04 15:38:03 +0000

    release 4.3.4

---
+++ tags/2.0.0/Locales/deDE.lua
@@ -0,0 +1,35 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("iGear", "deDE")
+if not L then return end
+
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["BackSlot"] = "Umhang"
+L["ChestSlot"] = "Brust"
+L["FeetSlot"] = "F\195\188\195\159e"
+L["Finger0Slot"] = "Finger"
+L["Finger1Slot"] = "Finger"
+L["HandsSlot"] = "H\195\164nde"
+L["HeadSlot"] = "Kopf"
+L["LegsSlot"] = "Beine"
+L["MainHandSlot"] = "Waffenhand"
+L["NeckSlot"] = "Hals"
+L["RangedSlot"] = "Fernkampf"
+L["Repair Costs"] = "Repkosten"
+L["SecondaryHandSlot"] = "Nebenhand"
+L["ShoulderSlot"] = "Schulter"
+L["Trinket0Slot"] = "Schmuck"
+L["Trinket1Slot"] = "Schmuck"
+L["WaistSlot"] = "Taille"
+L["WristSlot"] = "Handgelenke"
+
+L["Equip"] = "Ausr\195\188stung"
+L["Inventory"] = "Inventar"
+L["In Bags"] = "In Taschen"
+L["At Bank"] = "Auf Bank"
+L["Total Cost"] = "Kosten Gesamt"
+
+L["Eq"] = "Eq"
+L["En"] = "Vz"
+L["Ge"] = "Ge"
+--@end-do-not-package@
