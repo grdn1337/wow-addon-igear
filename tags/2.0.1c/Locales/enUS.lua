629ed18e988618fd940ac779ab714ce7cec46042
grdn <grdn@8ac71d79-9e84-4f73-86c8-66763a5b8044>
2012-07-04 15:55:46 +0000

    Cataclysm

---
+++ tags/2.0.1c/Locales/enUS.lua
@@ -0,0 +1,33 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("iGear", "enUS", true)
+
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["HeadSlot"] = "Head"
+L["NeckSlot"] = "Neck"
+L["ShoulderSlot"] = "Shoulder"
+L["BackSlot"] = "Back"
+L["ChestSlot"] = "Chest"
+L["WristSlot"] = "Wrist"
+L["HandsSlot"] = "Hands"
+L["WaistSlot"] = "Waist"
+L["LegsSlot"] = "Legs"
+L["FeetSlot"] = "Feet"
+L["Finger0Slot"] = "Finger"
+L["Finger1Slot"] = "Finger"
+L["Trinket0Slot"] = "Trinket"
+L["Trinket1Slot"] = "Trinket"
+L["MainHandSlot"] = "Mainhand"
+L["SecondaryHandSlot"] = "Offhand"
+L["RangedSlot"] = "Ranged"
+
+L["Equip"] = "Equip"
+L["Inventory"] = "Inventory"
+L["In Bags"] = "In Bags"
+L["At Bank"] = "At Bank"
+L["Total Cost"] = "Total Cost"
+
+L["Eq"] = "Eq"
+L["En"] = "En"
+L["Ge"] = "Ge"
+--@end-do-not-package@
