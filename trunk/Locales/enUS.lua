f5360fcb8d5e42da48433a6b4a28912d83b731c9
grdn <grdn@8ac71d79-9e84-4f73-86c8-66763a5b8044>
2012-07-12 19:58:53 +0000

    - Integration of iLib - Another great code overhaul - Added more localization

--- trunk/Locales/enUS.lua
+++ trunk/Locales/enUS.lua
@@ -22,13 +22,25 @@ L["MainHandSlot"] = "Mainhand"
 L["SecondaryHandSlot"] = "Offhand"
 L["RangedSlot"] = "Ranged"
 
-L["Equip"] = "Equip"
-L["Inventory"] = "Inventory"
-L["In Bags"] = "In Bags"
-L["At Bank"] = "At Bank"
-L["Total Cost"] = "Total Cost"
+L["Equip"] = true
+L["Inventory"] = true
+L["In Bags"] = true
+L["At Bank"] = true
+L["Total Cost"] = true
 
-L["Eq"] = "Eq"
-L["En"] = "En"
-L["Ge"] = "Ge"
+L["Eq"] = true
+L["En"] = true
+L["Ge"] = true
+
+L["Who is paying the bill?"] = true
+L["Auto Repair"] = true
+L["Try Guild Repair"] = true
+L["If Action is set to Auto Repair, trys using Guild money for repairs. If it fails, falls back to your money!"] = true
+L["Action"] = true
+L["Popup Dialog"] = true
+L["Conflicts"] = true
+L["Missing Equip"] = true
+L["Missing Enchants"] = true
+L["Missing Gems"] = true
+L["Required level in order to check for equip conflicts"] = true
 --@end-do-not-package@
